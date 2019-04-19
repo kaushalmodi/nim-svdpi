9107b1bbce1a47b2c34dc99ee2581b8772abbbfc
Kaushal Modi <kaushal.modi@gmail.com>
2019-04-19 00:35:07 -0400

    Procs to convert to/from Nim seqs and SV dynamic arrays

--- src/svdpi.nim
+++ src/svdpi.nim
@@ -23,3 +23,21 @@ type
     bval*: uint32
 
 cImport(cSearchPath("svdpi.h"))
+
+## Accessibility procs
+
+proc svSeqToSVDynArr1*[T](s: seq[T]; dynArrPtr: svOpenArrayHandle) =
+  ## Write a Nim sequence to a one-dimensional SV dynamic array.
+  if dynArrPtr != nil: # Do nothing if the dynamic array is not allocated on the SV side.
+    for idx, val in s:
+      let
+        dynArrElemPtr = cast[ptr T](svGetArrElemPtr1(dynArrPtr, idx.cint))
+      dynArrElemPtr[] = val
+
+proc svSVDynArr1ToSeq*[T](dynArrPtr: svOpenArrayHandle): seq[T] =
+  ## Write a one-dimensional SV dynamic array to a Nim sequence.
+  if dynArrPtr != nil: # Do nothing if the dynamic array is not allocated on the SV side.
+    for idx in 0.cint ..< svLength(dynArrPtr, 1):
+      let
+        dynArrElemPtr = cast[ptr T](svGetArrElemPtr1(dynArrPtr, idx))
+      result.add(dynArrElemPtr[])
